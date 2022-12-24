9cbda2bcad0d40e9e575c4cb05abe25f4d335f70
saimohanreddy98 <saimohanreddy98@gmail.com>
2022-12-24 21:22:34 +0530

    done

--- App.js
+++ App.js
@@ -1,39 +1,12 @@
-const input0 = document.querySelector("#zero");
-const input1 = document.querySelector("#one");
-const input2 = document.querySelector("#two");
-const input3 = document.querySelector("#three");
-const input4 = document.querySelector("#four");
-const input5 = document.querySelector("#five");
-
-const inputs = [input0, input1, input2, input3, input4, input5];
-
-const moveFocusToNextInput = (eventOriginationInputNumber) => {
-  if (eventOriginationInputNumber === 5) {
-    return;
-  }
-
-  inputs[eventOriginationInputNumber + 1].focus();
-};
-
-const moveFocusToPreviousInput = (eventOriginationInputNumber) => {
-  if (eventOriginationInputNumber === 0) {
-    return;
-  }
-
-  inputs[eventOriginationInputNumber - 1].focus();
-};
-
-const onInputChange = (event) => {
-  const inputNumber = parseInt(event.target.getAttribute("data-number"));
-
-  if (event.key === "Backspace") {
-    moveFocusToPreviousInput(inputNumber);
-    return;
-  }
-
-  moveFocusToNextInput(inputNumber);
-};
-
-inputs.forEach((input) => {
-  input.addEventListener("keyup", onInputChange);
-});
+const codes = document.querySelectorAll('.code')
+codes[0].focus()
+codes.forEach((code, idx) => {
+   code.addEventListener('keydown', (e) => {
+       if(e.key >= 0 && e.key <=9) {
+           codes[idx].value = ''
+           setTimeout(() => codes[idx + 1].focus(), 10)
+       } else if(e.key === 'Backspace') {
+           setTimeout(() => codes[idx - 1].focus(), 10)
+       }
+   })
+})
